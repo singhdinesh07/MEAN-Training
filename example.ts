b10a5b640977f48a0bc8b7b5c6ba4231977e8295
Singh, Dinesh <dinesh.singh@marsh.com>
2025-05-20 13:47:33 +0530

    Added all the js and ts files

--- example.ts
+++ example.ts
@@ -2,3 +2,4 @@ import {Products}  from "./Products";
 //import Products from "./Products";
 console.log("hello");
 var prod : Products = new Products(1,"TestProd","","TestDesc",12344,1);
+console.log(prod);
